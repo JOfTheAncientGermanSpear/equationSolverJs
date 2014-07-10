de6cd3ea80a2ddb1c57a126ac4a5eeab5902e3f8
jOfTheAncientGermanSpear <*>
2014-07-10 14:34:00 -0400

    fixed spacing

--- test/equationSolverTest.js
+++ test/equationSolverTest.js
@@ -14,24 +14,24 @@ function simpleDensity(){
 };
 
 describe('Equation', function(){
-    describe('calculate unknown param', function(){
-        it('should return the function to calculate the unknown param', function(){
-            var fnMap = {
-                a: function(params){
-                    return params.b * 2;
-                },
-                b: function(params) {
-                    return params.a * 3;
-                }
-            };
-            var params = {
-                b: 3
-            };
-            var expected = 6;
-            var result = equation._calculateUnknownParam(fnMap, params);
-            assert.equal(expected, result.a);
-        })
-    });
+	describe('calculate unknown param', function(){
+		it('should return the function to calculate the unknown param', function(){
+			var fnMap = {
+				a: function(params){
+					return params.b * 2;
+				},
+				b: function(params) {
+					return params.a * 3;
+				}
+			};
+			var params = {
+				b: 3
+			};
+			var expected = 6;
+			var result = equation._calculateUnknownParam(fnMap, params);
+			assert.equal(expected, result.a);
+		})
+	});
 	describe('equation generator', function(){
 		it('should generate density calculator that calculates missing param', function(){
 			var density = simpleDensity();
